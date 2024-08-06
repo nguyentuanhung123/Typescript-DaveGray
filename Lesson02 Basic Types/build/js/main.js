c2454a324c1205ad9090ccc324ac4877de1af15b
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-08-06 18:17:12 +0700

    lesson 02: basic types

---
+++ Lesson02 Basic Types/build/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Van Halen';
+const sum = (a, b) => {
+    return a + b;
+};
+let postId; // Union Types
+let isActive; // Union Types
---
+++ Lesson02 Basic Types/src/main.ts
@@ -0,0 +1,17 @@
+let myName: string = 'Dave';
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Van Halen';
+
+const sum = (a: number, b: string) => {
+    return a + b;
+}
+
+let postId: string | number; // Union Types
+let isActive: number | boolean; // Union Types
+
